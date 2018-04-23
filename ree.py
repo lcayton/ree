5d2ad4ea7c19555449cda3d7ca0fe6376e363cf2
Lawrence Cayton <mail@lcayton.com>
2018-04-22 19:16:20 -0700

    Linting and code cleanup.

--- ree.py
+++ ree.py
@@ -1,20 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.optimize, scipy.spatial
+import scipy.optimize
+import scipy.spatial
 import warnings
+
 
 def cmds(D):
     n, _ = D.shape
-    H = np.eye(n) - np.ones((n, n))/n
-    B = -H.dot(D**2).dot(H)/2
+    H = np.eye(n) - np.ones((n, n)) / n
+    B = -H.dot(D**2).dot(H) / 2
     X = _gram_to_config(B)
     return X
+
 
 def ree_error(D, Dp):
     # Returns the sum of the abolute elementwise
     # differences of D and Dp. Note: assumes that D
     # and Dp and both *squared* distance matrices.
     return np.sum(np.abs(D - Dp))
+
 
 def _ree_grad(B, Dsq):
     # Computes the REE subgradient for a gram matrix B,
@@ -28,7 +32,12 @@ def _ree_grad(B, Dsq):
     return G
 
 
-def ree(D, max_num_its=100000, no_line_search=False, init_zero=True, verbose=True):
+def ree(
+        D,
+        max_num_its=100000,
+        no_line_search=False,
+        init_zero=True,
+        verbose=True):
     n, _ = D.shape
     D = 0.5 * (D + D.T)
     Dsq = D * D
@@ -42,7 +51,7 @@ def ree(D, max_num_its=100000, no_line_search=False, init_zero=True, verbose=Tru
 
     it = 0
     not_done = True
-    c = 0.1# np.float32(n)
+    c = 0.1
     best_err = np.inf
     while not_done:
         def _error(b):
@@ -59,18 +68,20 @@ def ree(D, max_num_its=100000, no_line_search=False, init_zero=True, verbose=Tru
         alpha = None
         if not no_line_search:
             with warnings.catch_warnings():
-                warnings.filterwarnings('ignore', 'The line search algorithm did not converge')
-                (alpha, fc, gc, new_fval, old_fval, new_slope) = scipy.optimize.line_search(_error, _grad, B.reshape(-1), G)
+                warnings.filterwarnings(
+                    'ignore', 'The line search algorithm did not converge')
+                (alpha, fc, gc, new_fval, old_fval, new_slope) = scipy.optimize.line_search(
+                    _error, _grad, B.reshape(-1), G)
 
         if not alpha:
-            alpha = c / np.sqrt(it+1)
+            alpha = c / np.sqrt(it + 1)
             # Alternative rule: gamma / norm(G)
             # alpha = 3 / np.linalg.norm(G)
 
         Bnew = B + alpha * _vec_to_sq_matrix(G)
 
         def _project(C):
-            C = C - np.sum(C)/np.float(C.size)
+            C = C - np.sum(C) / np.float(C.size)
             X = _gram_to_config(C)
             return X.dot(X.T)
 
@@ -78,35 +89,40 @@ def ree(D, max_num_its=100000, no_line_search=False, init_zero=True, verbose=Tru
         B = Bnew
         new_err = ree_error(gram_dist(B), Dsq)
         best_err = np.min([best_err, new_err])
-        if verbose and not it%10:
-            print "it {}; error {:.2f} (best={:.2f}) [alpha={}]".format(it, new_err, best_err, alpha)
+        if verbose and not it % 10:
+            print(
+                "it {}; error {:.2f} (best={:.2f}) [alpha={}]".format(
+                    it, new_err, best_err, alpha))
         it += 1
         not_done &= (it < max_num_its)
 
     X = _gram_to_config(B)
     return X
 
+
 def _gram_to_config(B):
     evals, evecs = np.linalg.eigh(B)
-    idx   = np.argsort(evals)[::-1]
+    idx = np.argsort(evals)[::-1]
     evals = evals[idx]
-    evecs = evecs[:,idx]
+    evecs = evecs[:, idx]
     w, = np.where(evals > 0)
     L = np.diag(np.sqrt(evals[w]))
-    U = evecs[:,w]
+    U = evecs[:, w]
     X = U.dot(L)
     return X
+
 
 def _vec_to_sq_matrix(b):
     nsq = b.shape[0]
     n = int(np.sqrt(nsq))
     return b.reshape(n, n)
 
+
 def gram_dist(B):
-    n, _ = B.shape
     # Returns matrix D s.t. D_ij = B_ii + B_jj - B_ij - B_ji
+    n, _ = B.shape
     b = np.diag(B).reshape(-1, 1)  # make column vector out of B_ii's
-    o = np.ones((n,1))
+    o = np.ones((n, 1))
     return b.dot(o.T) + o.dot(b.T) - B - B.T
 
 
@@ -117,13 +133,7 @@ def plot(X, names):
         ax.annotate(n, (X[i, 0], X[i, 1]))
     plt.show()
 
+
 def dists(X):
     d = scipy.spatial.distance.pdist(X)
     return scipy.spatial.distance.squareform(d)
-
-if __name__ == '__main__':
-    D, names = cities_data()
-    X = cmds(D)
-    print('error = {}'.format(np.linalg.norm(D-dists(X))))
-    plot(X, names)
-
--- test_ree.py
+++ test_ree.py
@@ -5,9 +5,11 @@ import pytest
 
 SEED = 297
 
+
 @pytest.fixture
 def random_state():
     return np.random.RandomState(SEED)
+
 
 @pytest.fixture
 def cities_data():
@@ -15,25 +17,38 @@ def cities_data():
     # Derived from http://polisci.msu.edu/jacoby/iu/mds2012/citydists/citydists.txt.
     # Symmetrized.
     D = np.array(
-        [[0,0.587,1.212,0.701,1.936,0.604,0.748,2.139,2.182,0.543],
-         [0.587,0,0.920,0.940,1.745,1.188,0.713,1.858,1.737,0.597],
-         [1.212,0.920,0,0.879,0.831,1.726,1.631,0.949,1.021,1.494],
-         [0.701,0.940,0.879,0,1.374,0.968,1.420,1.645,1.891,1.220],
-         [1.936,1.745,0.831,1.374,0,2.339,2.451,0.347,0.959,2.300],
-         [0.604,1.188,1.726,0.968,2.339,0,1.092,2.594,2.734,0.923],
-         [0.748,0.713,1.631,1.420,2.451,1.092,0,2.571,2.408,0.205],
-         [2.139,1.858,0.949,1.645,0.347,2.594,2.571,0,0.678,2.442],
-         [2.182,1.737,1.021,1.891,0.959,2.734,2.408,0.678,0,2.329],
-         [0.543,0.597,1.494,1.229,2.300,0.923,0.205,2.442,2.329,0]])
+        [[0, 0.587, 1.212, 0.701, 1.936, 0.604, 0.748, 2.139, 2.182, 0.543],
+         [0.587, 0, 0.920, 0.940, 1.745, 1.188, 0.713, 1.858, 1.737, 0.597],
+         [1.212, 0.920, 0, 0.879, 0.831, 1.726, 1.631, 0.949, 1.021, 1.494],
+         [0.701, 0.940, 0.879, 0, 1.374, 0.968, 1.420, 1.645, 1.891, 1.220],
+         [1.936, 1.745, 0.831, 1.374, 0, 2.339, 2.451, 0.347, 0.959, 2.300],
+         [0.604, 1.188, 1.726, 0.968, 2.339, 0, 1.092, 2.594, 2.734, 0.923],
+         [0.748, 0.713, 1.631, 1.420, 2.451, 1.092, 0, 2.571, 2.408, 0.205],
+         [2.139, 1.858, 0.949, 1.645, 0.347, 2.594, 2.571, 0, 0.678, 2.442],
+         [2.182, 1.737, 1.021, 1.891, 0.959, 2.734, 2.408, 0.678, 0, 2.329],
+         [0.543, 0.597, 1.494, 1.229, 2.300, 0.923, 0.205, 2.442, 2.329, 0]])
     D_clean = 0.5 * (D + D.T)
-    names = ('atl', 'chi', 'den', 'hou', 'LA', 'mia', 'NYC', 'SF', 'sea', 'WDC')
+    names = (
+        'atl',
+        'chi',
+        'den',
+        'hou',
+        'LA',
+        'mia',
+        'NYC',
+        'SF',
+        'sea',
+        'WDC')
     return D_clean, names
+
 
 def random_config(n, d, random_state):
     return random_state.rand(n, d)
 
+
 def rel_ree_error(Dfound, D):
     return ree_error(Dfound, D) / np.sum(np.abs(D))
+
 
 def test_cities(cities_data):
     D, _ = cities_data
@@ -43,13 +58,15 @@ def test_cities(cities_data):
     Xree = ree(D, 100)
     assert rel_ree_error(dists(Xree), D) < .05
 
+
 def test_gram_dist(random_state):
     X = random_config(20, 3, random_state)
     B = X.dot(X.T)
     D = dists(X)
     Dg = gram_dist(B)
 
-    assert_almost_equal(D*D,  Dg)
+    assert_almost_equal(D * D, Dg)
+
 
 def test_cmds(random_state):
     X = random_config(30, 4, random_state)
@@ -58,6 +75,7 @@ def test_cmds(random_state):
     assert np.linalg.matrix_rank(Xmds.dot(Xmds.T)) == 4
     assert_almost_equal(dists(Xmds), D)
 
+
 def test_ree_edm(random_state):
     # See if REE can recover an actual euclidean matrix
     # with low error.
@@ -65,6 +83,7 @@ def test_ree_edm(random_state):
     D = dists(X)
     Xree = ree(D, max_num_its=1000)
     assert rel_ree_error(dists(Xree), D) < .05
+
 
 @pytest.fixture
 def sparse_perturb(random_state):
@@ -80,11 +99,12 @@ def sparse_perturb(random_state):
 
     for p in range(num_perturb):
         i = random_state.randint(1, n)
-        j = random_state.randint(i+1, n+1)
+        j = random_state.randint(i + 1, n + 1)
         Dp[i, j] += noise[i, j]
         Dp[j, i] += noise[j, i]
 
     return D, Dp
+
 
 def test_ree_sparse_perturbed(sparse_perturb):
     D, Dp = sparse_perturb
